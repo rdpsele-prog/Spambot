de6bc3ff21a4f250b6d0ab8c5734a9e934f3a6b1
Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ <111850575+Shivam892483@users.noreply.github.com>
2023-06-07 12:19:04 +0530

    Update help.py

--- AltBots/modules/help.py
+++ AltBots/modules/help.py
@@ -3,7 +3,7 @@ from telethon import events, Button
 from config import X1, X2, X3, X4, X5, X6, X7, X8, X9, X10, SUDO_USERS, CMD_HNDLR as hl
 
 
-HELP_STRING = f"★ sʜɪᴠᴀᴍ sᴘᴀᴍ ʙᴏᴛ ʜᴇʟᴘ ᴍᴇɴᴜ\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ: @PyXen**"
+HELP_STRING = f"★ sʜɪᴠᴀᴍ sᴘᴀᴍ ʙᴏᴛ ʜᴇʟᴘ ᴍᴇɴᴜ\n\n» **ᴄʟɪᴄᴋ ᴏɴ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ꜰᴏʀ ʜᴇʟᴘ**\n» **ᴅᴇᴠᴇʟᴏᴘᴇʀ : @Shivam_jaiswal0011**"
 
 HELP_BUTTON = [
     [
